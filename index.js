6f52017663f41a3386f9f641be0b27b667647675
Anthony Rodriguez-Ortiz <anthonyero@gmail.com>
2024-02-18 22:30:32 -0600

    Added a colon for the license question prompt

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const questions = [
     },
     {
         type: "list",
-        message: "License",
+        message: "License:",
         name: "license",
         choices: ["None","Apache License 2.0", "GNU General Public License v3.0", "MIT License", 'BSD 2-Clause "Simplified"', 'BSD 3-Clause "New" or "Revised" License',
         "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Eclipse Public License 2.0", "GNU Affero General Public License v3.0",
