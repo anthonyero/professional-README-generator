8cadc7d2cfd0f025ac46c08e0d9883187ced9cac
Anthony Rodriguez-Ortiz <anthonyero@gmail.com>
2024-02-18 22:49:42 -0600

    Invoked the Inquirer's prompt function to iterate through the questions array

--- index.js
+++ index.js
@@ -58,7 +58,14 @@ const questions = [
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((response) => {
+        
+    })
+
+}
 
 // Function call to initialize app
 init();
